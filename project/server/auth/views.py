10b7ffcfcf2b927549fd3d691ad9b017a1e95ff7
Addin Kilau <addinesia@gmail.com>
2018-01-08 16:50:06 +0700

    tambah list product

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -232,6 +232,45 @@ class ProductAPI(MethodView):
             }
             return make_response(jsonify(responseObject)), 403
 
+    def get(self):
+        # get the auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Bearer token malformed.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = Product.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                p = Product.query.all()
+                responseObject = {
+                    'status': 'success',
+                    'data': {
+                        'id': p.id,
+                        'nama': p.nama,
+                        'harga': p.harga,
+                        'jumlah': p.jumlah
+                    }
+                }
+                return make_response(jsonify(responseObject)), 200
+            responseObject = {
+                'status': 'fail',
+                'message': resp
+            }
+            return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 401
 
 
 # define the API resources
@@ -266,4 +305,9 @@ auth_blueprint.add_url_rule(
     '/product',
     view_func=product_view,
     methods=['POST']
+)
+auth_blueprint.add_url_rule(
+    '/product/list',
+    view_func=product_view,
+    methods=['GET']
 )
