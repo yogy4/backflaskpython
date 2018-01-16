3cb06934083d1352d0961fc9d841782ecd5499c2
Addin Kilau <addinesia@gmail.com>
2018-01-16 07:19:14 +0700

    ubah get product view tambah not

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -252,7 +252,7 @@ class ProductAPI(MethodView):
         if auth_token:
             resp = User.decode_auth_token(auth_token)
             # isi = []
-            if not isinstance(resp, str) and isi:
+            if not isinstance(resp, str) and not isi:
             # p = Product.get_all()
                 # isi = []
                 p = Product.query.all()
